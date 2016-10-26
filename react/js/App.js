961315fcb6bb1596ddb18d8f3409b44ef3e1ac8d
quiaro <quiaro@gmail.com>
2016-10-26 08:01:47 -0600

    react: manage component state with setState

--- react/js/App.js
+++ react/js/App.js
@@ -3,8 +3,21 @@ import React from 'react';
 // const App = () => <h1>Stateless function component</h1>
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { txt: 'Component state text' }
+  }
+  update(e) {
+    this.setState({txt: e.target.value })
+  }
   render() {
-    return <h1>{this.props.txt}</h1>
+    return (
+        <div>
+          <h1>{this.props.txt}</h1>
+          <p>{this.state.txt}</p>
+          <input type='text' onChange={this.update.bind(this)}/>
+        </div>
+    )
   }
 }
 
