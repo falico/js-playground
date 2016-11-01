2fb094c06867bb6b3c5475fd6f605528cf6cd308
quiaro <quiaro@gmail.com>
2016-11-01 10:37:30 -0600

    redux: initial set up for sample app

---
+++ redux/js/todo/todo.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+//
+// ReactDOM.render(<App cat={12} />, document.getElementById('app'));
+
+console.log('Hello world');
---
+++ redux/webpack.config.js
@@ -0,0 +1,29 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    todo: './js/todo/todo.js'
+  },
+  output: {
+    path: __dirname + '/static',
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devServer: {
+    host: '0.0.0.0',
+    port: 1357
+  }
+}
