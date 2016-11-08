1459f9c4f7a4fa57606eac2ed8df7ee77cb1c332
quiaro <quiaro@gmail.com>
2016-11-08 09:16:22 -0600

    redux: replace return statements with object expressions

--- redux/js/todo/actions/todo.js
+++ redux/js/todo/actions/todo.js
@@ -2,40 +2,32 @@ import { ACTIONS } from './constants'
 
 let todoId = 1;
 
-export const addTodo = (text) => {
-  return {
+export const addTodo = (text) => ({
     type: ACTIONS.ADD_TODO,
     payload: {
       id: todoId++,
       text,
       completed: false
     }
-  }
-}
+  })
 
-export const removeTodo = (text) => {
-  return {
+export const removeTodo = (text) => ({
     type: ACTIONS.REMOVE_TODO,
     payload: {
       text
     }
-  }
-}
+  })
 
-export const toggleTodo = (id) => {
-  return {
+export const toggleTodo = (id) => ({
     type: ACTIONS.TOGGLE_TODO,
     payload: {
       id
     }
-  }
-}
+  })
 
-export const setVisibilityFilter = (filter) => {
-  return {
+export const setVisibilityFilter = (filter) => ({
     type: ACTIONS.SET_VISIBILITY_FILTER,
     payload: {
       filter
     }
-  }
-}
+  })
--- redux/js/todo/containers/FilterLink.js
+++ redux/js/todo/containers/FilterLink.js
@@ -2,19 +2,15 @@ import { connect } from 'react-redux'
 import Link from '../components/Link'
 import { setVisibilityFilter } from '../actions/todo'
 
-const mapStateToProps = (state, ownProps) => {
-  return {
-    active: ownProps.filter === state.visibilityFilter
-  }
-}
+const mapStateToProps = (state, ownProps) => ({
+  active: ownProps.filter === state.visibilityFilter
+})
 
-const mapDispatchToProps = (dispatch, ownProps) => {
-  return {
-    onClick: () => {
-      dispatch(setVisibilityFilter(ownProps.filter))
-    }
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick() {
+    dispatch(setVisibilityFilter(ownProps.filter))
   }
-}
+})
 
 const FilterLink = connect(
   mapStateToProps,
