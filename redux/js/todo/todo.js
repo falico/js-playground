73b23241608b639fe39ce66cd5de64c25096ee88
quiaro <quiaro@gmail.com>
2016-11-08 15:11:39 -0600

    redux: avoid saving state successively by using throttle

--- redux/js/todo/todo.js
+++ redux/js/todo/todo.js
@@ -5,16 +5,17 @@ import { createStore } from 'redux'
 import todoApp from './reducers/index'
 import App from './components/App'
 import * as State from './utils/state'
+import throttle from 'lodash/throttle'
 
 const initialState = State.load();
 let store = createStore(todoApp, initialState);
 
-store.subscribe(() => {
+store.subscribe(throttle(() => {
   let state = store.getState();
   State.save({
     todos: state.todos
   })
-});
+}, 1000));
 
 render(
   <Provider store={store}>
