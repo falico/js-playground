2f7bbf24260ee45c5d0e0004a019a44a4066b6f1
quiaro <quiaro@gmail.com>
2016-11-23 13:52:46 -0600

    redux: rename error component

--- redux/js/todo/components/errors/__tests__/FetchTodosError.js
+++ redux/js/todo/components/errors/__tests__/FetchTodosError.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { shallow } from 'enzyme';
-import FetchTodosError from '../FetchTodos'
+import FetchTodosError from '../FetchTodosError'
 
 describe('FetchTodosError', () => {
 
